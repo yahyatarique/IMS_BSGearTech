c053501d2dec2bab646494a2e7bd507ccc203ec8
Suhail-007 <mohdsuhail51998@gmail.com>
2025-11-09 20:52:52 +0530

    add suspense in inventory

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,7 +1,10 @@
 import InventoryPage from '@/components/pages/inventory/inventory';
+import { Suspense } from 'react';
 
 export default function Inventory() {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
       <InventoryPage />
+    </Suspense>
   );
 }
